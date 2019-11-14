1427eafeb0ae6c7714026f25850b4a5ed768666d
Mike D <mdrevet2712@gmail.com>
2019-11-14 14:43:31 +0100

    Enorme Commit
    - Ajout d'un npc basique
    - Reecriture completes des colliders
    - Ajout d'une barre de vie au dessus du npc
    - Ajout d'une Enumeration pour les type d'attaques

---
+++ RedStick Redemption/Assets/Scripts/PlayerAttackEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackEnum : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private enum PlayerAttack { kick, lowkick, punch, uppercut, flyingKick }
+    public PlayerAttack PlayerAttackType { get; set; }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
