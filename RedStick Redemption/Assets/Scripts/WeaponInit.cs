159b8b6e841c02bb9af39544a9460d6cf2f90738
Lucas <lucas.secret@etu.univ-lyon1.fr>
2019-11-20 13:45:41 +0100

    Work on get Weapon by broking box

---
+++ RedStick Redemption/Assets/Scripts/WeaponInit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void init()
+    {
+        transform.SetParent(null);     
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            transform.SetParent(GameObject.Find("MainDroite").transform);
+        }
+    }
+}
